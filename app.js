1ed06037d7d3c6e9f1b196c131cfac22b8364309
hijihyun <67703614+hijihyun@users.noreply.github.com>
2021-06-20 09:52:54 +0900

    로딩

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 let $studySelect = null; // 클릭할 때마다 저장되어야 하므로 전역변수
 let $quizSelect = null;
 
-const $studyFilters = document.querySelector(".btn-group").children;
+const $studyFilters = document.querySelector("#study-filter").children;
 const studyFiltersLength = $studyFilters.length;
 const $quizFilters = document.querySelector("#quiz-filter").children;
 const quizFiltersLength = $quizFilters.length;
 
+const $studyLoading = document.querySelector("#study-loading");
+const $studyTable = document.querySelector("#studyTable");
+const $quizLoading = document.querySelector("#quiz-loading");
+const $quizTable = document.querySelector("#quizTable");
+
 // ========== 1. 각 탭이 선택되면 선택된 탭 class(active) 적용 ==========
 function addStudyFilterActive(event) {
+  studyLoading();
+  setTimeout(studyLoaded, 500);
+
   $studyFilters[0].className = $studyFilters[0].className.replace(
     " active",
     ""
@@ -24,6 +32,8 @@ function addStudyFilterActive(event) {
 }
 
 function addQuizFilterActive(event) {
+  quizLoading();
+  setTimeout(quizLoaded, 500);
   $quizFilters[0].className = $quizFilters[0].className.replace(" active", ""); // default active 제거
 
   if ($quizSelect) {
@@ -35,10 +45,34 @@ function addQuizFilterActive(event) {
   $quizSelect = el;
 }
 
-// ========== 2. 이벤트 처리 ==========
 for (let i = 0; i < studyFiltersLength; i++) {
   $studyFilters[i].addEventListener("click", addStudyFilterActive);
 }
+
 for (let i = 0; i < quizFiltersLength; i++) {
   $quizFilters[i].addEventListener("click", addQuizFilterActive);
 }
+
+// ========== 2. 로딩 ==========
+function studyLoading() {
+  $studyLoading.style.display = "";
+  $studyTable.style.display = "none";
+}
+
+function studyLoaded() {
+  $studyLoading.style.display = "none";
+  $studyTable.style.display = "";
+}
+
+function quizLoading() {
+  $quizLoading.style.display = "";
+  $quizTable.style.display = "none";
+}
+
+function quizLoaded() {
+  $quizLoading.style.display = "none";
+  $quizTable.style.display = "";
+}
+
+studyLoaded();
+quizLoaded();
